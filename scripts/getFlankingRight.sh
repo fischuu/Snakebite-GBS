cab31d0d27e6a297c3b293d5c739fc96b0560323
Daniel Fischer <git@danielfischer.name>
2022-12-13 18:24:44 +0200

    typo fixed

--- scripts/getFlankingRight.sh
+++ scripts/getFlankingRight.sh
@@ -1,3 +1,3 @@
 input=$1
 output=$2
-cut -f2,3,5,6 $input | awk  'BEGIN { OFS = "\t" } NR>0{a=0; b=0; for (i=1;i<=NF;i++) if ($i < a || i == 1)a = $i; else if($i > b|| i == 1)b = $i; print $1,b, b+6}' > $output
+cut -f2,3,5,6 $input | awk 'BEGIN { OFS = "\t" } NR>0{a=0; b=0; for (i=1;i<=NF;i++) if ($i < a || i == 1)a = $i; else if($i > b|| i == 1)b = $i; print $1,b, b+6}' > $output
