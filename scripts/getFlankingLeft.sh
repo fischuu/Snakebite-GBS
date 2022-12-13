a5659c4ad6e03e6d9b4d3c0f700b686c0d1f0df9
Daniel Fischer <git@danielfischer.name>
2022-12-13 20:11:19 +0200

    more updates

--- scripts/getFlankingLeft.sh
+++ scripts/getFlankingLeft.sh
@@ -1,3 +1,3 @@
 input=$1
 output=$2
-cut -f2,3,5,6 $input | awk 'BEGIN { OFS = "\t" } NR>0{a=0; b=0; for (i=1;i<=NF;i++) if ($i < a || i == 1) a = $i; else if($i > b|| i == 1)b = $i; print $0,a-6, a}' > $output
+cut -f1,2,3,5,6 $input | awk 'BEGIN { OFS = "\t" } {a=0; b=0; for (i=1;i<=NF;i++) if ($i < a || i == 1) a = $i; else if($i > b|| i == 1)b = $i; print $1,a-6, a}' > $output
