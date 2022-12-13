5ed28a3c9ad326f1a02ed2c76bad0d36c2c983d3
Daniel Fischer <git@danielfischer.name>
2022-12-13 10:42:18 +0200

    flanking bed files are now tab-delimted, as reqired by bedtools

--- GBS-pipeline.smk
+++ GBS-pipeline.smk
@@ -12,8 +12,8 @@ import yaml
 ##### Natural Resources Institute Finland (Luke)
 ##### This pipeline is build upon the the GBS-SNP-CROP pipeline:
 ##### https://github.com/halelab/GBS-SNP-CROP
-##### Version: 0.17.20
-version = "0.17.20"
+##### Version: 0.17.22
+version = "0.17.22"
 
 ##### set minimum snakemake version #####
 min_version("6.0")
--- scripts/getFlankingLeft.sh
+++ scripts/getFlankingLeft.sh
@@ -1,3 +1,3 @@
 input=$1
 output=$2
-cut -f2,3,5,6 $input | awk  'NR>1{a=0; b=0; for (i=1;i<=NF;i++) if ($i < a || i == 1)a = $i; else if($i > b|| i == 1)b = $i; print a-6, a}' > $output
+cut -f2,3,5,6 $input | awk 'BEGIN { OFS = "\t" } NR>1{a=0; b=0; for (i=1;i<=NF;i++) if ($i < a || i == 1)a = $i; else if($i > b|| i == 1)b = $i; print a-6, a}' > $output
