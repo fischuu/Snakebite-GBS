1b16414826be6688b766090662e9332dafb7299d
Daniel Fischer <git@danielfischer.name>
2022-12-22 18:45:25 +0200

    In silico report added

--- GBS-pipeline.smk
+++ GBS-pipeline.smk
@@ -12,8 +12,8 @@ import yaml
 ##### Natural Resources Institute Finland (Luke)
 ##### This pipeline is build upon the the GBS-SNP-CROP pipeline:
 ##### https://github.com/halelab/GBS-SNP-CROP
-##### Version: 0.17.42
-version = "0.17.42"
+##### Version: 0.17.50
+version = "0.17.50"
 
 ##### set minimum snakemake version #####
 min_version("6.0")
--- scripts/inSilicoFasta.R
+++ scripts/inSilicoFasta.R
@@ -1,6 +1,9 @@
 library("SimRAD")
 library("GenomicTools")
 
+minLength <- as.numeric(minLength)-1
+maxLength <- as.numeric(maxLength)+1
+
 # Adjust the paths
 if(substr(refGenome.file,1,1)!="/") refGenome.file <- file.path(projFolder, refGenome.file)
 
@@ -18,10 +21,10 @@ ddout <- insilico.digest(fasta, cut_site_5prime1 = enz1.1,
                          cut_site_5prime2 = enz2.1,
                          cut_site_3prime2 = enz2.2)
 
+ddout.selected <- size.select(ddout, min.size = minLength, max.size = maxLength, graph = FALSE)
 
 names(ddout) <- paste0("> Location", 1:length(ddout))
 exportFA(ddout, file=full.file)
 
-ddout.selected <- size.select(ddout, min.size = minLength, max.size = maxLength, graph = FALSE)
 names(ddout.selected) <- paste0("> SelectedLocation", 1:length(ddout.selected))
 exportFA(ddout.selected, file=selected.file)
