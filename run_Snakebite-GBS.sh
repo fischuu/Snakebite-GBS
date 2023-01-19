99c97af32a725f8dcadb677cb43d239bfbf8a7e2
Daniel Fischer <git@danielfischer.name>
2023-01-19 14:12:39 +0200

    Update and rename run_Pipeline-GBS.sh to run_Snakebite-GBS.sh

--- run_Snakebite-GBS.sh
+++ run_Snakebite-GBS.sh
@@ -1,4 +1,4 @@
-pipelineFolder="~/Snakemake/Pipeline-GBS"
+pipelineFolder="~/git/Snakebite-GBS"
 projectFolder="/scratch/project_2001746/GBS_Example"
 
 module load snakemake
@@ -9,17 +9,17 @@ export APPTAINER_CACHEDIR="/scratch/project_2001746/tmp"
 mkdir -p $APPTAINER_TMPDIR
 
 # Create the rulegraph
-snakemake -s $pipelineFolder/Pipeline-GBS.smk \
-          --configfile $projectFolder/Pipeline-GBS_config.yaml \
+snakemake -s $pipelineFolder/Snakebite-GBS.smk \
+          --configfile $projectFolder/Snakebite-GBS_config.yaml \
           --rulegraph | dot -T png > $projectFolder/workflow.png
 
-snakemake -s $pipelineFolder/Pipeline-GBS.smk \
+snakemake -s $pipelineFolder/Snakebite-GBS.smk \
           -j 150 \
           --use-singularity \
           --singularity-args "-B /scratch,/projappl,/users,/dev/shm:/tmp,/run:/run" \
-          --configfile $projectFolder/Pipeline-GBS_config.yaml \
+          --configfile $projectFolder/Snakebite-GBS_config.yaml \
           --latency-wait 60 \
-          --cluster-config $projectFolder/Pipeline-GBS_server-config.yaml \
+          --cluster-config $projectFolder/Snakebite-GBS_server-config.yaml \
           --cluster "sbatch -t {cluster.time} --account={cluster.account} --gres=nvme:{cluster.nvme} --job-name={cluster.job-name} --tasks-per-node={cluster.ntasks} --cpus-per-task={cluster.cpus-per-task} --mem-per-cpu={cluster.mem-per-cpu} --mail-user={cluster.mail-user} --mail-type={cluster.mail-type} -p {cluster.partition} -D {cluster.working-directory --parsable}" \
           --scheduler greedy \
 	  --cluster-cancel scancel \
