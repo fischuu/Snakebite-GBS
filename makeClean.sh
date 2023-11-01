a9b44d09e61bfe299d4e65a747e0fe8376bf6b7a
Daniel Fischer <git@danielfischer.name>
2023-11-01 15:23:20 +0200

    negative values avoided that created an error in the past

--- makeClean.sh
+++ makeClean.sh
@@ -1,3 +1,4 @@
+rm *.out
 rm -rf BAM/
 rm -rf benchmark/
 rm -rf BWA/
@@ -8,9 +9,16 @@ rm -rf QC/
 rm -rf SAM/
 rm -rf FASTQ/CONCATENATED/
 rm -rf FASTQ/TRIMMED/
+rm -rf FASTQ/CUTADAPT/
 rm -rf VCF/
 rm -rf logs/
 rm -rf QUANTIFICATION/
 rm -rf Stringtie/
 rm -rf .snakemake/
 rm mockEvalReport.html
+rm References/full_inSilico_reference.fa*
+rm References/sizeSelected_inSilico_reference.fa*
+rm References/AB_contigs.txt
+rm barcodesID.txt
+rm IndexMockReferenceBWA.log
+rm -rf REPORTS/
--- scripts/getFastaFromVCF.sh
+++ scripts/getFastaFromVCF.sh
@@ -7,5 +7,6 @@ output2=$4
 inputvcf=$5
 
 # Prepare the loci file
-sed '/^#/ d' $input | cut -f1,2 | awk -v s=$flanking -v OFS= '{print $1,":", $2-s,"-",$2+s}' > $output1
+sed '/^#/ d' $input | cut -f1,2 | awk -v s=$flanking -v OFS= '{if ($2-s < 1) print $1,":", 1,"-",$2+s; else print $1,":", $2-s,"-",$2+s}' > $output1
+
 samtools faidx $inputvcf -n 300 -r $output1 > $output2
